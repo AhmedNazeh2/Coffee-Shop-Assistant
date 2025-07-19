9a463f27b09f226e2b04fae471adb39cc681d71c
Eslam Mohamed <eslam3012mohamed@gmail.com>
2025-07-19 20:22:42 +0300

    Define LLM configuration and agent state structure

---
+++ state.py
@@ -0,0 +1,7 @@
+from typing import TypedDict, Sequence, Annotated
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    customer_id: int
